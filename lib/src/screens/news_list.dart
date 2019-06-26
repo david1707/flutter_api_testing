1bbe478ebfc294708a2b4e458a0a5f8bf3f0dd30
david1707 <DavidAlacant@gmail.com>
2019-06-26 21:10:22 +0200

    Added BLOC stuff I barely understand

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+    bloc.fetchTopIds();
+
+    // THIS IS BAD, DON'T DO THIS
     return Scaffold(
       appBar: AppBar(
         title: Text('Top news'),
       ),
-      body: Text('Show a list'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
+        if(!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text('${snapshot.data[index]}');
+          }
+        );
+      }
     );
   }
 }
