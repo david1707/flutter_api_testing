dc89c28b70240e8c4931d8f35c158a6c494611b1
david1707 <DavidAlacant@gmail.com>
2019-07-02 00:19:19 +0200

    Added second page and fetching individual news

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -6,9 +6,10 @@ import '../widgets/refresh.dart';
 class NewsList extends StatelessWidget {
   Widget build(context) {
     final bloc = StoriesProvider.of(context);
-    bloc.fetchTopIds();
 
     // THIS IS BAD, DON'T DO THIS
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top news'),
